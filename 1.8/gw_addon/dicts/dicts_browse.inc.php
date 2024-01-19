83ea0383b60254bb7264d39e672ac011fbcae383
zdenop <zdenop@gmail.com>
2024-01-19 22:11:36 +0100

    fix each() in gw_addon/dicts/dicts_browse.inc.php

--- 1.8/gw_addon/dicts/dicts_browse.inc.php
+++ 1.8/gw_addon/dicts/dicts_browse.inc.php
@@ -133,102 +133,102 @@ if ($this->gw_this['vars']['w1'] )
 {
 	$arAlltopics = ctlgGetTree( $ar, $this->gw_this['vars']['w1'] );
 }
-while (is_array($arAlltopics) && list($kp, $tp) = each ($arAlltopics))
-{
-	/* Topic selected */
-	if (isset($arDictMap[$tp]) && is_array($arDictMap[$tp]))
-	{
-		$cnt_dict = 0;
-		foreach ($arDictMap[$tp] as $k => $arV)
+if (is_array($arAlltopics) ) {	/* Topic selected */
+	foreach ($arAlltopics as $kp => $tp) {
+		if (isset($arDictMap[$tp]) && is_array($arDictMap[$tp]))
 		{
-			$is_allow_terms = 0;  
-			if ( $this->oSess->is('is-sys-settings')
-				|| $this->oSess->is('is-dicts')
-				|| (isset($ar_allowed_dicts[$arV['id']]) && $this->oSess->is('is-dicts-own') )
-			)
+			$cnt_dict = 0;
+			foreach ($arDictMap[$tp] as $k => $arV)
 			{
-				$is_allow_terms = 1; 
-			}
-			/* */
-			$bgc = $cnt_dict % 2 ? $this->ar_theme['color_2'] : $this->ar_theme['color_1'];
-			$menu = array();
+				$is_allow_terms = 0;
+				if ( $this->oSess->is('is-sys-settings')
+					|| $this->oSess->is('is-dicts')
+					|| (isset($ar_allowed_dicts[$arV['id']]) && $this->oSess->is('is-dicts-own') )
+				)
+				{
+					$is_allow_terms = 1;
+				}
+				/* */
+				$bgc = $cnt_dict % 2 ? $this->ar_theme['color_2'] : $this->ar_theme['color_1'];
+				$menu = array();
 
-			$this->str .= '<div style="border-top:1px #EEE solid;padding:5px;background:'.$bgc.';text-align:'.$this->sys['css_align_left'].'">';
-			$this->str .= '<div class="xw">'.$arV['title'].'</div>';
-			$this->str .= '<div class="xt">'.$arV['announce'].'</div>';
-			$this->str .= '</div>';
+				$this->str .= '<div style="border-top:1px #EEE solid;padding:5px;background:'.$bgc.';text-align:'.$this->sys['css_align_left'].'">';
+				$this->str .= '<div class="xw">'.$arV['title'].'</div>';
+				$this->str .= '<div class="xt">'.$arV['announce'].'</div>';
+				$this->str .= '</div>';
 
-			$this->str .= '<table style="border-bottom:1px #EEE solid;" border="0" width="100%" cellpadding="3" cellspacing="1">';
-			$this->str .= '<tbody><tr style="background:'.$bgc.';text-align:'.$this->sys['css_align_left'].';vertical-align:top">';
-			$this->str .= '<td style="width:1%" class="xt nobr"><span class="gray">ID: </span>'.$arV["id"].'</td>';
-			$this->str .= '<td class="actions-third"><span>';
-			/* 1.8.7: added link to search for all terms */
-			($is_allow_terms) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH .'&'. GW_TARGET.'='.GW_T_DICTS. '&q=*&srch[in]=1&id=' . $arV['id'], '…&#160;'.$this->oL->m('3_browse'), $this->oL->m('terms').': '.$this->oL->m('3_browse') ) : '';
-			($is_allow_terms) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_EDIT .'&'. GW_TARGET.'='.GW_T_DICTS. '&tid='.$arV['id'].'&id='.$arV['id'], '±&#160;'.$this->oL->m('3_edit'), $this->oL->m('1335').': '.$this->oL->m('3_edit') ) : '';
-			($is_allow_terms) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_ADD .'&'. GW_TARGET.'='.GW_T_TERMS .'&id='.$arV['id'], '+&#160;' . $this->oL->m('3_add_term'), $this->oL->m('terms').': '.$this->oL->m('3_add') ) : '';
-			($is_allow_terms && $this->oSess->is('is-terms-export')) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_EXPORT .'&'. GW_TARGET.'='.GW_T_TERMS. '&id='.$arV['id'], $this->oL->m('3_export'), $this->oL->m('terms').': '.$this->oL->m('3_export')  ) : '';
-			($is_allow_terms && $this->oSess->is('is-terms-import')) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_IMPORT .'&'. GW_TARGET.'='.GW_T_TERMS. '&id='.$arV['id'], $this->oL->m('3_import'), $this->oL->m('terms').': '.$this->oL->m('3_import')  ) : '';
+				$this->str .= '<table style="border-bottom:1px #EEE solid;" border="0" width="100%" cellpadding="3" cellspacing="1">';
+				$this->str .= '<tbody><tr style="background:'.$bgc.';text-align:'.$this->sys['css_align_left'].';vertical-align:top">';
+				$this->str .= '<td style="width:1%" class="xt nobr"><span class="gray">ID: </span>'.$arV["id"].'</td>';
+				$this->str .= '<td class="actions-third"><span>';
+				/* 1.8.7: added link to search for all terms */
+				($is_allow_terms) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH .'&'. GW_TARGET.'='.GW_T_DICTS. '&q=*&srch[in]=1&id=' . $arV['id'], '…&#160;'.$this->oL->m('3_browse'), $this->oL->m('terms').': '.$this->oL->m('3_browse') ) : '';
+				($is_allow_terms) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_EDIT .'&'. GW_TARGET.'='.GW_T_DICTS. '&tid='.$arV['id'].'&id='.$arV['id'], '±&#160;'.$this->oL->m('3_edit'), $this->oL->m('1335').': '.$this->oL->m('3_edit') ) : '';
+				($is_allow_terms) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_ADD .'&'. GW_TARGET.'='.GW_T_TERMS .'&id='.$arV['id'], '+&#160;' . $this->oL->m('3_add_term'), $this->oL->m('terms').': '.$this->oL->m('3_add') ) : '';
+				($is_allow_terms && $this->oSess->is('is-terms-export')) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_EXPORT .'&'. GW_TARGET.'='.GW_T_TERMS. '&id='.$arV['id'], $this->oL->m('3_export'), $this->oL->m('terms').': '.$this->oL->m('3_export')  ) : '';
+				($is_allow_terms && $this->oSess->is('is-terms-import')) ? $menu[] = $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_IMPORT .'&'. GW_TARGET.'='.GW_T_TERMS. '&id='.$arV['id'], $this->oL->m('3_import'), $this->oL->m('terms').': '.$this->oL->m('3_import')  ) : '';
 
-			$this->str .= implode(' ', $menu);
-			$this->str .= '</span>';
-			$this->str .= '</td>';
+				$this->str .= implode(' ', $menu);
+				$this->str .= '</span>';
+				$this->str .= '</td>';
 
-			/* The number of terms */
-			$this->str .= '<td class="actions-third" style="width:20%;text-align:'.$this->sys['css_align_right'].'">';
-			if ($arV['int_terms'])
-			{
-				$this->str .= $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH.'&id='.$arV['id'].'&q=*&srch[in]=103&t=dicts', '<span class="green">'.$this->oFunc->number_format($arV['int_terms'], 0, $this->oL->languagelist('4')).'</span>', $this->oL->m('1320'));
-			}
-			else
-			{
-				$this->str .= '<del title="'.$this->oL->m('1320').'">0</del>';
-			}
-			$this->str .= ' / ';
-			if ($arV['int_terms_total']-$arV['int_terms'])
-			{
-				$this->str .= $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH.'&id='.$arV['id'].'&q=*&srch[in]=100&t=dicts', '<span class="red">'.$this->oFunc->number_format($arV['int_terms_total']-$arV['int_terms'], 0, $this->oL->languagelist('4')).'</span>', $this->oL->m('srch_7'));
-			}
-			else
-			{
-				$this->str .= '<del title="'.$this->oL->m('srch_7').'">0</del>';
-			}
-			$this->str .= ' / ';
-			if ($arV['int_terms_total'])
-			{
-				$this->str .= $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH.'&id='.$arV['id'].'&q=*&srch[in]=1&t=dicts', $this->oFunc->number_format($arV['int_terms_total'], 0, $this->oL->languagelist('4')), $this->oL->m('total'));
-			}
-			else
-			{
-				$this->str .= '<del title="'.$this->oL->m('total').'">0</del>';
-			}
-			$this->str .= '</td>';
-			/* Interface language */
-			$this->str .= '<td class="xq" style="width:15%;text-align:center"><span class="gray">' . ($arV['lang'] ? $languagelist[$arV['lang']] : $languagelist[$this->sys['locale_name']] ) . "</span></td>";
+				/* The number of terms */
+				$this->str .= '<td class="actions-third" style="width:20%;text-align:'.$this->sys['css_align_right'].'">';
+				if ($arV['int_terms'])
+				{
+					$this->str .= $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH.'&id='.$arV['id'].'&q=*&srch[in]=103&t=dicts', '<span class="green">'.$this->oFunc->number_format($arV['int_terms'], 0, $this->oL->languagelist('4')).'</span>', $this->oL->m('1320'));
+				}
+				else
+				{
+					$this->str .= '<del title="'.$this->oL->m('1320').'">0</del>';
+				}
+				$this->str .= ' / ';
+				if ($arV['int_terms_total']-$arV['int_terms'])
+				{
+					$this->str .= $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH.'&id='.$arV['id'].'&q=*&srch[in]=100&t=dicts', '<span class="red">'.$this->oFunc->number_format($arV['int_terms_total']-$arV['int_terms'], 0, $this->oL->languagelist('4')).'</span>', $this->oL->m('srch_7'));
+				}
+				else
+				{
+					$this->str .= '<del title="'.$this->oL->m('srch_7').'">0</del>';
+				}
+				$this->str .= ' / ';
+				if ($arV['int_terms_total'])
+				{
+					$this->str .= $this->oHtml->a($this->sys['page_admin'].'?'.GW_ACTION.'='.GW_A_SEARCH.'&id='.$arV['id'].'&q=*&srch[in]=1&t=dicts', $this->oFunc->number_format($arV['int_terms_total'], 0, $this->oL->languagelist('4')), $this->oL->m('total'));
+				}
+				else
+				{
+					$this->str .= '<del title="'.$this->oL->m('total').'">0</del>';
+				}
+				$this->str .= '</td>';
+				/* Interface language */
+				$this->str .= '<td class="xq" style="width:15%;text-align:center"><span class="gray">' . ($arV['lang'] ? $languagelist[$arV['lang']] : $languagelist[$this->sys['locale_name']] ) . "</span></td>";
 
-			/* 1.8.7: Turn on/off */
-			$href_onoff = $this->sys['page_admin'] . '?'.GW_ACTION.'='.GW_A_EDIT.'&'.GW_TARGET.'='.$this->gw_this['vars'][GW_TARGET].'&tid='.$arV['id'].'&id='.$arV['id'].'&w1='.$this->gw_this['vars']['w1'];
-			$this->str .= '<td class="actions-third" style="width:1%;text-align:center">';
-			if ($is_allow_terms)
-			{
-				$this->str .= ($arV['is_active'] 
-							? $this->oHtml->a($href_onoff.'&mode=off', '<span class="green">'.$this->oL->m('is_1').'</span>')
-							: $this->oHtml->a($href_onoff.'&mode=on', '<span class="red">'.$this->oL->m('is_0').'</span>', $this->oL->m('1057') ) );
-			}
-			else
-			{
-				$this->str .= '&#160;'. ($arV['is_active'] 
-							? '<del><span class="green">'.$this->oL->m('is_1').'</span></del>'
-							: '<del><span class="red">'.$this->oL->m('is_0').'</span></del>') . '&#160;';
-			}
-			$this->str .= '</td>';
+				/* 1.8.7: Turn on/off */
+				$href_onoff = $this->sys['page_admin'] . '?'.GW_ACTION.'='.GW_A_EDIT.'&'.GW_TARGET.'='.$this->gw_this['vars'][GW_TARGET].'&tid='.$arV['id'].'&id='.$arV['id'].'&w1='.$this->gw_this['vars']['w1'];
+				$this->str .= '<td class="actions-third" style="width:1%;text-align:center">';
+				if ($is_allow_terms)
+				{
+					$this->str .= ($arV['is_active']
+								? $this->oHtml->a($href_onoff.'&mode=off', '<span class="green">'.$this->oL->m('is_1').'</span>')
+								: $this->oHtml->a($href_onoff.'&mode=on', '<span class="red">'.$this->oL->m('is_0').'</span>', $this->oL->m('1057') ) );
+				}
+				else
+				{
+					$this->str .= '&#160;'. ($arV['is_active']
+								? '<del><span class="green">'.$this->oL->m('is_1').'</span></del>'
+								: '<del><span class="red">'.$this->oL->m('is_0').'</span></del>') . '&#160;';
+				}
+				$this->str .= '</td>';
 
-			$this->str .= '</tr>';
-			$this->str .= '</tbody></table>';
-#			$this->str .= '<div style="font-size:1px;height:2px;background:'.$this->ar_theme['color_1'].'"></div>';
-			++$cnt_dict;
-		} // foreach $arDictMap
-	} // isset parent
-} // while
+				$this->str .= '</tr>';
+				$this->str .= '</tbody></table>';
+	#			$this->str .= '<div style="font-size:1px;height:2px;background:'.$this->ar_theme['color_1'].'"></div>';
+				++$cnt_dict;
+			} // foreach $arDictMap
+		} // isset parent
+	} // foreach $arAlltopics
+}
 unset($ar);
 unset($arDictMap);
 
